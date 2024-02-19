48d524fedb560560dbf0f175eb41b74cf5228c81
moneprz <145432190+moneprz@users.noreply.github.com>
2024-02-19 15:46:48 +0100

    Update qrCode.php

--- qrCode.php
+++ qrCode.php
@@ -1,11 +1,37 @@
 <?php
 require 'vendor/autoload.php';
 
-use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
 
-$phoneNumber = '+43 1 22 33 444';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $phoneNumber = $_POST['phone'];
 
-$qrCode = new QrCode($phoneNumber);
+    $result = Builder::create()
+        ->writer(new PngWriter())
+        ->writerOptions([])
+        ->data($phoneNumber)
+        ->encoding(new Encoding('UTF-8'))
+        ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+        ->size(300)
+        ->margin(10)
+        ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+        ->logoPath(__DIR__.'/assets/symfony.png')
+        ->logoResizeToWidth(50)
+        ->logoPunchoutBackground(true)
+        ->labelText($phoneNumber)
+        ->labelFont(new NotoSans(20))
+        ->labelAlignment(LabelAlignment::Center)
+        ->validateResult(false)
+        ->build();
 
-header('Content-Type: '.$qrCode->getContentType());
-echo $qrCode->writeString();
+    header('Content-Type: '.$result->getMimeType());
+    echo $result->getString();
+} else {
+    header('Location: index.php');
+    exit();
+}
+?>
